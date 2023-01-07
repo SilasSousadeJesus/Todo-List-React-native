c5145da8aa5e414d1378ac4ef56b0372f11b0704
silassousa <silassousadejesus@gmail.com>
2023-01-07 16:14:08 -0300

    feat() criando projetos e seus primeiros componentes

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import { todosData } from "../../data/todos";
+import { FlatList, View, Text } from "react-native";
+import Todo from "./Todo";
+
+export default function TodoList() {
+  return (
+    <FlatList
+      data={todosData}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => <Todo {...item} />}
+    />
+  );
+}
